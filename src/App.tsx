c3e68cfba664b4cb986126fa23910dfb9a6b5ff0
Philip Moir <phil@captur.ai>
2025-05-01 00:37:18 +0100

    Add title and description to default page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Helmet, HelmetProvider } from '@dr.pogodin/react-helmet';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Landing from './routes/Landing';
@@ -6,13 +7,25 @@ import DashboardHome from './routes/dashboard/DashboardHome';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<DashboardHome />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <Helmet>
+        <title>
+          YouthKitbag React - Inventory, Trade, Aquire, Reporting for all your
+          youth kit
+        </title>
+        <meta
+          name="description"
+          content="Your ultimate toolkit for youth activities."
+        />
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<DashboardHome />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
