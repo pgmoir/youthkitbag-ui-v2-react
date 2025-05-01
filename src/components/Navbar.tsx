e94da8e4992bcc257c5ec58d43c070d971c7a06f
Philip Moir <phil@captur.ai>
2025-05-01 09:46:34 +0100

    Add roboto font and navbar with logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
 import { JSX } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar(): JSX.Element {
-  return <nav>YouthKitbag</nav>;
+  return (
+    <nav className="nav-container">
+      <Link to="/" className="nav-logo">
+        YouthKitbag
+      </Link>
+    </nav>
+  );
 }
 
 export default Navbar;
